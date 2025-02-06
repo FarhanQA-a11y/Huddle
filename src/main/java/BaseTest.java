9560f7e2e485968fe60e319fa31effe3fe84e7d6
FarhanQA-a11y <farhankazmi53@gmail.com>
2025-02-06 23:38:21 +0530

    cross browser code pushed in branch last

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,29 +1,52 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	 protected static WebDriver driver;
 
-	 @BeforeSuite
-	    public void setUp() {
+	 @Parameters("browser")
+	 @BeforeTest
+	  
+	    public void setUp(String browser) {
+		 if (browser.equalsIgnoreCase("chrome")) {
+	            WebDriverManager.chromedriver().setup();
+	            driver = new ChromeDriver();
+	        } else if (browser.equalsIgnoreCase("firefox")) {
+	            WebDriverManager.firefoxdriver().setup();
+	            driver = new FirefoxDriver();
+	        } else if (browser.equalsIgnoreCase("edge")) {
+	            WebDriverManager.edgedriver().setup();
+	            driver = new EdgeDriver();
+	        } else {
+	            throw new IllegalArgumentException("Invalid browser: " + browser);
+	        }
 	        // Setup ChromeDriver using WebDriverManager
-	        WebDriverManager.chromedriver().setup();
-	        driver = new ChromeDriver();
+//	        WebDriverManager.chromedriver().setup();
+//	        driver = new ChromeDriver();
 	        
 	        // Maximize browser window
 	        driver.manage().window().maximize();
 	        driver.get("https://automationpanda.com/");   
+	        System.out.println("Opened website in " + browser);
 	 }
 
-//	    @AfterClass
-//	    public void tearDown() {
-//	        if (driver != null) {
-//	            driver.quit();
-//	        }
-//	    }
+	    @AfterTest
+	    public void tearDown() {
+	        if (driver != null) {
+	            driver.quit();
+	            System.out.println("Closed browser");
+	        }
+	    }
 }
--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,20 +1,26 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 	protected WebDriver driver;
 	
+	@FindBy(xpath = "(//input[@id='subscribe-field'])[1]")
+	private WebElement searchButton;
+	
 	public HomePage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 	
 	 public void clickSearchButton() throws InterruptedException {
 		 System.out.println("Clicked on farh");
-	        WebElement searchButton = driver.findElement(By.xpath("(//input[@id='subscribe-field'])[1]"));
+	        
 	        searchButton.click();
 	        Thread.sleep(300);
-	        searchButton.sendKeys("farhan");
+	        searchButton.sendKeys("farhan@yopmail.com");
 	        System.out.println("Clicked on Search button.");
 	    }
 
